4d19df61c26d2647d1cf74038d2041c7d7a3ca29
Natalya Okuneva <okuneva_natasha@mail.ru>
2019-11-10 01:59:52 +0500

    feat: pencil tool functionality

--- 1. palette/script.js
+++ 1. palette/script.js
@@ -1,4 +1,4 @@
-const data1 = [
+const data = [
     ["#00BCD4", "#FFEB3B", "#FFEB3B", "#00BCD4"],
     ["#FFEB3B", "#FFC107", "#FFC107", "#FFEB3B"],
     ["#FFEB3B", "#FFC107", "#FFC107", "#FFEB3B"],
@@ -8,16 +8,56 @@ const data1 = [
 
 const canvas_4x4 = document.querySelector('#canvas_4x4');
 
-const ctx1 = canvas_4x4.getContext("2d");
+const ctx = canvas_4x4.getContext('2d');
 
-let width1 = data1[0].length;
-let height1 = data1.length;
-let scaleX1 = canvas_4x4.width / width1;
-let scaleY1 = canvas_4x4.height / height1;
+let width = data[0].length;
+let height = data.length;
+let scaleX = canvas_4x4.width / width;
+let scaleY = canvas_4x4.height / height;
 
-for (let row = 0; row < height1; row++) {
-    for (let col = 0; col < width1; col++) {
-        ctx1.fillStyle = data1[row][col];
-        ctx1.fillRect(col * scaleX1, row * scaleY1, scaleX1, scaleY1);
+for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+        ctx.fillStyle = data[row][col];
+        ctx.fillRect(col * scaleX, row * scaleY, scaleX, scaleY);
     }
 }
+
+let selectedTool;
+const selectedTools = (tool) => {
+    for (let i = 0; i < tool.length; i++) {
+        tool[i].addEventListener('click', function () {
+            for (k = 0; k < tool.length; k++) {
+                tool[k].removeAttribute('checked');
+            }
+            selectedTool = tool[i].value;
+            tool[i].setAttribute('checked', '');
+        });
+    }
+}
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function pencilDraw(e) {
+    if (selectedTool === "pencil") {
+        if (!isDrawing) return;
+        let x = Math.floor(e.offsetX / scaleX);
+        let y = Math.floor(e.offsetY / scaleY);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(x * scaleX, y * scaleY, scaleX, scaleY);
+    }
+}
+
+canvas_4x4.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    pencilDraw(e);
+});
+canvas_4x4.addEventListener('mousemove', pencilDraw);
+document.addEventListener('mouseup', () => isDrawing = false);
+
+
+
+
+let tool = document.querySelectorAll('.tool');
+selectedTools(tool);
