661ac919fc65f8c5a2d330611ae19f08736aefa5
Natalya Okuneva <okuneva_natasha@mail.ru>
2019-11-10 21:10:52 +0300

    feat: add bucket functionality

--- 1. palette/script.js
+++ 1. palette/script.js
@@ -10,23 +10,28 @@ const canvas_4x4 = document.querySelector('#canvas_4x4');
 
 const ctx = canvas_4x4.getContext('2d');
 
-let width = data[0].length;
-let height = data.length;
-let scaleX = canvas_4x4.width / width;
-let scaleY = canvas_4x4.height / height;
 
-for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-        ctx.fillStyle = data[row][col];
-        let m = ctx.fillStyle;
-        ctx.fillRect(col * scaleX, row * scaleY, scaleX, scaleY);
+let scaleX = canvas_4x4.width / data[0].length;
+let scaleY = canvas_4x4.height / data.length;
+
+function fillCanvas(data) {
+    let width = data[0].length;
+    let height = data.length;
+    let scaleX = canvas_4x4.width / width;
+    let scaleY = canvas_4x4.height / height;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            ctx.fillStyle = data[row][col];
+            ctx.fillRect(col * scaleX, row * scaleY, scaleX, scaleY);
+        }
     }
 }
 
+fillCanvas(data);
 // Select the tool
 
 let selectedTool = "pencil";
-let selectedColor = '#808080';
+let selectedColor = 'rgba(128,128,128,1)';
 
 const selectedTools = (tool) => {
     for (let i = 0; i < tool.length; i++) {
@@ -89,7 +94,67 @@ function pickColor(e) {
     let colorArray = ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data;
     selectedColor = `rgba(${colorArray[0]},${colorArray[1]},${colorArray[2]},${(colorArray[3] / 255)})`;
     currentColor.style.background = selectedColor;
+    console.log(selectedColor);
 }
+
+// Bucket 
+
+function getPixel(imageData, x, y) {
+    let valueX = Math.floor(x / scaleX);
+    let valueY = Math.floor(y / scaleY);
+    return imageData[valueY * 4 + valueX];
+}
+
+function setPixel(imageData, x, y, color) {
+    let valueX = Math.floor(x / scaleX);
+    let valueY = Math.floor(y / scaleY);
+    ctx.fillStyle = color;
+    ctx.fillRect(valueX * scaleX, valueY * scaleY, scaleX, scaleY);
+    imageData[valueY * 4 + valueX] = color;
+}
+
+function colorsMatch(a, b) {
+    return a === b;
+}
+
+function floodFill(ctx, x, y, fillColor) {
+    const bigImageData = ctx.getImageData(0, 0, canvas_4x4.width, canvas_4x4.width).data;
+    let imageData = [];
+    let stepYLimit = canvas_4x4.height * canvas_4x4.width * 4;
+    for (let stepY = 0; stepY < stepYLimit; stepY = stepY + canvas_4x4.width * scaleY * 4) {
+
+        for (let stepX = stepY; stepX < stepY + canvas_4x4.width * 4; stepX = stepX + scaleX * 4) {
+
+            imageData.push(`rgba(${bigImageData[stepX + 0]},${bigImageData[stepX + 1]},${bigImageData[stepX + 2]},${(bigImageData[stepX + 3] / 255)})`);
+        }
+    }
+    const targetColor = getPixel(imageData, x, y);
+    if (!colorsMatch(targetColor, fillColor)) {
+        fillPixel(imageData, x, y, targetColor, fillColor);
+    }
+
+    let finalImageData = [];
+    let cutImageData = imageData;
+    while (cutImageData.length > 0) {
+        finalImageData.push(cutImageData.splice(0, 4));
+    }
+    fillCanvas(finalImageData);
+}
+
+function fillPixel(imageData, x, y, targetColor, fillColor) {
+
+    const currentColor = getPixel(imageData, x, y);
+    if (colorsMatch(currentColor, targetColor)) {
+        setPixel(imageData, x, y, fillColor);
+        fillPixel(imageData, x + scaleX, y, targetColor, fillColor);
+        fillPixel(imageData, x - scaleX, y, targetColor, fillColor);
+        fillPixel(imageData, x, y + scaleY, targetColor, fillColor);
+        fillPixel(imageData, x, y - scaleY, targetColor, fillColor);
+
+    }
+
+}
+
 
 // Handle mouse events
 
@@ -100,11 +165,12 @@ canvas_4x4.addEventListener('mousedown', (e) => {
             pencilDraw(e);
             break;
         case 'paint-bucket':
-            console.log('paint-bucket', selectedTool);
+            let startX = e.offsetX;
+            let startY = e.offsetY;
 
+            floodFill(ctx, startX, startY, selectedColor);
             break;
         case 'color-picker':
-            console.log('color-picker', selectedTool);
             pickColor(e);
             break;
 
@@ -114,7 +180,7 @@ canvas_4x4.addEventListener('mousemove', pencilDraw);
 document.addEventListener('mouseup', () => isDrawing = false);
 
 
-
+// Handle keyboad events
 
 
 
